7557d5aeb92589f6454052fb233a36d47d131849
tarikbugrakarali <63560839+tarikbugrakarali@users.noreply.github.com>
2022-12-06 13:22:41 +0200

    MongoDb connection modules are implemented.

--- RecoModa/Backend/models/User.js
+++ RecoModa/Backend/models/User.js
@@ -1,1 +1,16 @@
 //Tarık
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        phoneNumber : { type: String, required: true },
+        role: {type: String},
+        favoriteProductList :{ type: Array },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
---
+++ RecoModa/Backend/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv/config';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const connection_url =  "mongodb+srv://tarik:" + process.env.MONGO_PASSWORD + "@cluster0.ymimfnq.mongodb.net/Shop?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log("Error on mongodb connect")
+        throw err;
+    }
+    console.log("Mongoose is connected");
+});
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
+app.use(cors({
+    credentials: false,
+    origin: "*"
+}));
+
+app.use(express.json());
+
+app.listen(PORT,()=>{
+    console.log(`Example app listening on port ${PORT}`)
+})
