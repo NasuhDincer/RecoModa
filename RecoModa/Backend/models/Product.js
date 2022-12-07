39df9ad676852ceba9500b7a3436481afeeb9705
Zülal Nur Hıdıroğlu <nur.hidiroglu@ug.bilkent.edu.tr>
2022-12-07 16:14:36 +0300

    Update Product.js

--- RecoModa/Backend/models/Product.js
+++ RecoModa/Backend/models/Product.js
@@ -1,1 +1,18 @@
 //Zülal
+import mongoose from "mongoose";
+const Product = new mongoose.Schema(
+    {
+        productId: { type: String, required: true, unique: true },
+        productName: { type: String, required: true, unique: true },
+        price: { type: String, required: true },
+        description : { type: String, required: true },
+        catagory: {type: String, required: true},
+        color :{ type: Array, required: true },
+        owner: {type: String, required: true},
+        sizeInfo: {type: String, required: true},
+        inStock:{type: Boolean, required: true}
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("Product", Product);
