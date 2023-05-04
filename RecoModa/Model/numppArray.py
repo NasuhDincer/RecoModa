154be2c8236a141de1b5815c44768029956edc88
tarikbugrakarali <63560839+tarikbugrakarali@users.noreply.github.com>
2023-05-04 22:42:22 +0300

    small update

--- RecoModa/Model/numppArray.py
+++ RecoModa/Model/numppArray.py
@@ -6,12 +6,16 @@ import json
 arr = sys.argv[1]
 
 
-url = f'http://localhost:5000/api/post/embed' # replace with the URL of your Node.js server and the endpoint for updating posts
+url = f'http://localhost:5000/api/post/' # replace with the URL of your Node.js server and the endpoint for updating posts
 
 response = requests.get(url) # make a GET request to the API endpoint
-data = json.loads(response.content)
-my_array = np.array(data)
-print(my_array[0])
+posts = response.json() # parse the response as JSON
+arr = [[post['_id'], post['embedArray']] for post in posts]
+arr_str = json.dumps(arr)
+
+#data = json.loads(response.content)
+#my_array = np.array(data)
+print(arr_str)
 print("here")
 
 
