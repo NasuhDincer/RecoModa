d3c67388d95a896cce882c4ef917d5c35cd2c41b
hakangulcu <hakan.gulcu@ug.bilkent.edu.tr>
2023-05-05 17:03:37 +0300

    update recosimilar, pairwise

--- RecoModa/Model/recoSimilar.py
+++ RecoModa/Model/recoSimilar.py
@@ -11,20 +11,29 @@ try:
     url = f'http://localhost:5000/api/post/' # replace with the URL of your Node.js server and the endpoint for updating posts
     response = requests.get(url) # make a GET request to the API endpoint
     posts = response.json() # parse the response as JSON
-    arr = [[post['_id'], post['embedArray']] for post in posts]
+    #arr = [[post['_id'], post['embedArray']] for post in posts]
+    #df = pd.DataFrame(arr, columns=['_id', 'embedArray'])
+    df = pd.DataFrame([[post['_id'], post['embedArray']] for post in posts], columns=['_id', 'embedArray'])
+    df_emb = df['embedArray']
+    # df_emb = pd.DataFrame([[post['embedArray']] for post in posts], columns=['embedArray'])
+    df_embeds = df_emb.apply(pd.Series)
+    print(df_embeds)
 except json.JSONDecodeError as e:
     print(f"Error parsing JSON string: {e}")
 except Exception as e:
     print(f"Error: {e}")
 
+"""df = pd.DataFrame(((arr[1])),
+               columns =['embed'])
+print(df)
 df = np.zeros((len(arr),2048))
 for i in range(len(arr)):
     print(arr[i][0])
     print(arr[i][1][0:5])
-    df[i] = arr[i][1]
+    df[i] = arr[i][1]"""
 
 print("xxxxxx")
-
+print(df)
 from sklearn.metrics.pairwise import pairwise_distances
 
 
@@ -32,9 +41,11 @@ from sklearn.metrics.pairwise import pairwise_distances
 print("xxxx23xx")
 
 # Calcule DIstance Matriz
-cosine_sim = 1-pairwise_distances(df, metric='cosine')
+cosine_sim = 1-pairwise_distances(df_embeds, metric='cosine')
 cosine_sim[:4, :4]
-"""
+
+print("JSDSA")
+
 print("1======================")
 indices = pd.Series(range(len(df)), index=df.index)
 print(indices)
@@ -58,4 +69,4 @@ print("3======================")
 
 print(idx_rec)
 print(idx_sim)
-"""
+
