89da22dffbb6b60d5548f48153b5afb9e442b65c
tarikbugrakarali <63560839+tarikbugrakarali@users.noreply.github.com>
2023-05-07 19:10:38 +0300

    update

--- RecoModa/Frontend/components/Post.js
+++ RecoModa/Frontend/components/Post.js
@@ -1,94 +1,91 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
-export default class Post extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this._isMounted = false;
-  }
+const Post = (props) => {
+  useEffect(() => {
 
-  render() {
-    return (
+    console.log("img : " , JSON.stringify(props.mediaId.img))
+  }, []);
+return (<>
+  <View
+    style={{
+      padding: "2%",
+      width: `100%`,
+      height: undefined,
+      aspectRatio: 1,
+      marginBottom: "5%",
+    }}
+  >
+    <View
+      style={{
+        height: "10%",
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
       <View
         style={{
-          padding: "2%",
-          width: `100%`,
-          height: undefined,
-          aspectRatio: 1,
-          marginBottom: "5%",
+          height: "100%",
+          width: "80%",
+          flexDirection: "row",
+          alignItems: "center",
         }}
       >
-        <View
+        <Image
+          source={require("../Assets/user.png")}
           style={{
-            height: "10%",
-            width: "100%",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
+            width: undefined,
+            height: "100%",
+            aspectRatio: 1,
+            resizeMode: "contain",
           }}
-        >
-          <View
-            style={{
-              height: "100%",
-              width: "80%",
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <Image
-              source={require("../Assets/user.png")}
-              style={{
-                width: undefined,
-                height: "100%",
-                aspectRatio: 1,
-                resizeMode: "contain",
-              }}
-            />
-            <View style={{ width: "5%" }}></View>
-            <Text>Nasuh Dinçer</Text>
-          </View>
-          <View
-            style={{
-              height: "100%",
-              width: "20%",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Text>2 days ago</Text>
-          </View>
-        </View>
-        <View style={{ width: "100%", height: "60%" }}>
-          <Image
-            source={require("../Assets/logoicon.png")}
-            style={{
-              width: "100%",
-              height: undefined,
-              aspectRatio: 1.7,
-              resizeMode: "contain",
-            }}
-          />
-        </View>
-        <View style={{ width: "100%", height: "30%" }}>
-          <Text style={{ fontSize: 24 }}>First Post</Text>
-          <Text style={{ fontSize: 16, color: "gray" }}>
-            This is an explanation of the post!
-          </Text>
-          <TouchableOpacity
-            style={{ width: "100%", height: "40%", paddingTop: "5%" }}
-          >
-            <Image
-              source={require("../Assets/like.png")}
-              style={{
-                height: "100%",
-                aspectRatio: 1,
-                resizeMode: "contain",
-              }}
-            />
-          </TouchableOpacity>
-        </View>
+        />
+        <View style={{ width: "5%" }}></View>
+        <Text>{props.mediaId.mediaId}</Text>
       </View>
-    );
-  }
+      <View
+        style={{
+          height: "100%",
+          width: "20%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2 days ago</Text>
+      </View>
+    </View>
+    <View style={{ width: "100%", height: "60%" }}>
+      <Image
+        source={{ uri: `data:image/png;base64,${props.mediaId.img.data}` }}
+        style={{
+          width: "100%",
+          height: undefined,
+          aspectRatio: 1.7,
+          resizeMode: "contain",
+        }}
+      />
+    </View>
+    <View style={{ width: "100%", height: "30%" }}>
+      <Text style={{ fontSize: 24 }}>First Post</Text>
+      <Text style={{ fontSize: 16, color: "gray" }}>
+        This is an explanation of the post!
+      </Text>
+      <TouchableOpacity
+        style={{ width: "100%", height: "40%", paddingTop: "5%" }}
+      >
+        <Image
+          source={require("../Assets/like.png")}
+          style={{
+            height: "100%",
+            aspectRatio: 1,
+            resizeMode: "contain",
+          }}
+        />
+      </TouchableOpacity>
+    </View>
+  </View>
+  </>);
 }
+export default Post;
--- RecoModa/Frontend/screens/Home.js
+++ RecoModa/Frontend/screens/Home.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+/*import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Dimensions,
   FlatList,
@@ -18,7 +19,11 @@ export default class Home extends Component {
     };
     this._isMounted = false;
   }
-
+  useEffect(() => {
+    // this function will be called after the component is mounted or updated
+    console.log("Component did mount or update");
+  });
+  
   handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -82,6 +87,117 @@ export default class Home extends Component {
     );
   }
 }
+export const screenOptions = (navData) => {
+  return {
+    headerTitle: "SocialApp",
+    headerRight: () => (
+      <Ionicons
+        name={Platform.OS === "android" ? "md-chatboxes" : "ios-chatboxes"}
+        size={24}
+        color={Platform.OS === "android" ? "#fff" : Colors.brightBlue}
+        style={{ padding: 15, marginRight: 5 }}
+        onPress={() => navData.navigation.navigate("ChatList")}
+      />
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  list: {
+    width: "100%",
+  },
+  separator: {
+    marginTop: 10,
+  },
+});*/
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Dimensions,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Post from "../components/Post";
+
+const Home = (props) => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    // this function will be called after the component is mounted or updated
+    handleSubmit();
+    console.log("data : " , data)
+  }, []);
+
+  const handleSubmit = async () => {
+    
+    try {
+      const ipv4Address = "192.168.3.247";
+      const res = await axios.get(
+        "http://" + ipv4Address + ":5000/api/post/"
+      );
+      console.log(res.data[0].mediaId);
+      setData(res.data);
+    } catch (error) {
+      // handle error response
+      console.log(error);
+    }
+
+    //props.navigation.navigate("Home")
+  };
+
+  return (
+    <SafeAreaView style={{ width: "100%", height: "100%" }}>
+      <View
+        style={{
+          width: "100%",
+          height: "13%",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text
+          adjustsFontSizeToFit
+          style={{
+            fontSize: Dimensions.get("window").width / 12,
+            fontWeight: "bold",
+            marginTop: "5%",
+            textAlign: "center",
+          }}
+        >
+          RecoModa
+        </Text>
+      </View>
+      <FlatList
+        contentContainerStyle={{
+          width: "100%",
+        }}
+        data={data}
+        renderItem={({ item, index }) => <Post  mediaId={item}></Post>}
+        keyExtractor={(item, key) => item._id}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
 export const screenOptions = (navData) => {
   return {
     headerTitle: "SocialApp",
