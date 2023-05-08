1a3fbf614984eb47d4d4cdc17fa9ff98ff1ebca2
guvengergerli <guven.gergerli@ug.bilkent.edu.tr>
2023-05-09 02:16:11 +0300

    Update recoSimilar.py

--- RecoModa/Model/recoSimilar.py
+++ RecoModa/Model/recoSimilar.py
@@ -1,69 +1,50 @@
+import time
 import sys
 import requests
-import numpy as np
 import json
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics.pairwise import pairwise_distances
-#tf.version
 
-# EDIT BELOW
 
 post_id = str(sys.argv[1])
 count = int(str(sys.argv[2]))
-#print("count")
-#print(count)
 
 try:
+    #tic = time.perf_counter()
     url = f'http://localhost:5000/api/post/' # replace with the URL of your Node.js server and the endpoint for updating posts
     response = requests.get(url) # make a GET request to the API endpoint
+    #toc = time.perf_counter()
     posts = response.json() # parse the response as JSON
-    #arr = [[post['_id'], post['embedArray']] for post in posts]
-    #df = pd.DataFrame(arr, columns=['_id', 'embedArray'])
     df = pd.DataFrame([[post['_id'], post['embedArray']] for post in posts], columns=['_id', 'embedArray'])
+    """
     df_emb = df['embedArray']
     # df_emb = pd.DataFrame([[post['embedArray']] for post in posts], columns=['embedArray'])
     df_embeds = df_emb.apply(pd.Series)
-    #print(df_embeds)
+    """
+    df_embeds = pd.DataFrame(df['embedArray'].values.tolist())
 except json.JSONDecodeError as e:
     print(f"Error parsing JSON string: {e}")
 except Exception as e:
     print(f"Error: {e}")
 
-"""df = pd.DataFrame(((arr[1])),
-               columns =['embed'])
-print(df)
-df = np.zeros((len(arr),2048))
-for i in range(len(arr)):
-    print(arr[i][0])
-    print(arr[i][1][0:5])
-    df[i] = arr[i][1]"""
 
 df_ids = df['_id']
-
-
-#print("df:")
-#print(df)
-#print("df_embeds:")
-#print(df_embeds)
-
 idforsim = post_id
-#print("idforsim:")
-#print(idforsim)
 
 rowno = -1
 for i in range(len(df_ids)):
+    rowno = df_ids[df_ids == post_id].index[0]
+    """
     if idforsim == df_ids[i]:
         rowno = i
+    """
 
 # Calcule DIstance Matriz
 cosine_sim = 1-pairwise_distances(df_embeds, metric='cosine')
 cosine_sim[:4, :4]
 
-
 indices = pd.Series(range(len(df)), index=df.index)
-#print("indices:")
-#print(indices)
 
 # Function that get movie recommendations based on the cosine similarity score of movie genres
 def get_recommender(idx, df, top_n = 5):
@@ -78,13 +59,10 @@ def get_recommender(idx, df, top_n = 5):
 
 idx_rec, idx_sim = get_recommender(rowno, df, top_n = count)
 
-#print(idx_rec[0])
-#print(idx_sim)
-
+# Wrapping answers
 answerobject = []
 for i in range(count):
     answerobject.append(df_ids[idx_rec[i]])
-#answerobject = df_ids[idx_rec[0]]
-#print("answerobject:")
 print(answerobject)
 
+#print(toc - tic)
