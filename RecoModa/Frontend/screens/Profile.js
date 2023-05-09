8f40bfff73a33b9f5a6203bfb504607652b70ba8
Zülal Nur Hıdıroğlu <nur.hidiroglu@ug.bilkent.edu.tr>
2023-05-09 13:17:49 +0300

    Update Profile.js

--- RecoModa/Frontend/screens/Profile.js
+++ RecoModa/Frontend/screens/Profile.js
@@ -1,5 +1,7 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 import {
   Dimensions,
   FlatList,
@@ -13,101 +15,109 @@ import {
   useNavigation,
 } from "react-native";
 import Post from "../components/Post";
+const userId = toString(AsyncStorage.getItem("userId"));
+const Profile = (props) => {
+  const [data, setData] = useState({});
 
-export default class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      surname: "",
-      email: "",
-      password: "",
-      phoneNumber: "",
-    };
-    this._isMounted = false;
-  }
+  useEffect(() => {
+    // this function will be called after the component is mounted or updated
+    //handleSubmit();
+    console.log("userId : ", userId);
+  }, []);
 
-  getPersonalData = () => {};
+  const handleSubmit = async () => {
+    try {
+      const ipv4Address = "139.179.206.244";
+      const res = await axios.get(
+        "http://" + ipv4Address + ":5000/api/mediaprofile/"
+      );
+      console.log(res.data);
+      setData(res.data);
+    } catch (error) {
+      // handle error response
+      console.log(error);
+    }
 
-  render() {
-    return (
-      <SafeAreaView style={{ width: "100%", height: "100%" }}>
-        <View
-          style={{
-            width: "100%",
-            height: "7%",
-            paddingHorizontal: "5%",
-            flexDirection: "row",
-            justifyContent: "flex-end",
-            marginTop: "5%",
-            backgroundColor: "white",
-          }}
+    //props.navigation.navigate("Home")
+  };
+
+  return (
+    <SafeAreaView style={{ width: "100%", height: "100%" }}>
+      <View
+        style={{
+          width: "100%",
+          height: "7%",
+          paddingHorizontal: "5%",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          marginTop: "5%",
+          backgroundColor: "white",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("Settings")}
         >
-          <TouchableOpacity
-            onPress={() => this.props.navigation.navigate("Settings")}
-          >
-            <MaterialIcons name="settings" size={36} color="black" />
+          <MaterialIcons name="settings" size={36} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            style={styles.profileImage}
+            source={require("../Assets/user.png")}
+          />
+          <View style={styles.stats}>
+            <Text style={styles.stat}>{userId}</Text>
+            <Text style={styles.stat}>100 followers</Text>
+            <Text style={styles.stat}>200 following</Text>
+          </View>
+        </View>
+        <View style={styles.minibar}>
+          <TouchableOpacity style={styles.minibarItem}>
+            <MaterialIcons name="apps" size={36} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.minibarItem}>
+            <MaterialIcons name="checkroom" size={36} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.minibarItem}>
+            <MaterialIcons name="bookmark" size={36} color="black" />
           </TouchableOpacity>
         </View>
-        <View style={styles.container}>
-          <View style={styles.header}>
+        <View style={styles.postsContainer}>
+          <View style={styles.postsRow}>
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate("MyPost")}
+            >
+              <Image
+                style={styles.postImage}
+                source={require("../Assets/user.png")}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate("Settings")}
+            >
+              <Image
+                style={styles.postImage}
+                source={require("../Assets/user.png")}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.postsRow}>
             <Image
-              style={styles.profileImage}
+              style={styles.postImage}
               source={require("../Assets/user.png")}
             />
-            <View style={styles.stats}>
-              <Text style={styles.stat}>20 posts</Text>
-              <Text style={styles.stat}>100 followers</Text>
-              <Text style={styles.stat}>200 following</Text>
-            </View>
-          </View>
-          <View style={styles.minibar}>
-            <TouchableOpacity style={styles.minibarItem}>
-              <MaterialIcons name="apps" size={36} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.minibarItem}>
-              <MaterialIcons name="checkroom" size={36} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.minibarItem}>
-              <MaterialIcons name="bookmark" size={36} color="black" />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.postsContainer}>
-            <View style={styles.postsRow}>
-              <TouchableOpacity
-                onPress={() => this.props.navigation.navigate("MyPost")}
-              >
-                <Image
-                  style={styles.postImage}
-                  source={require("../Assets/user.png")}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => this.props.navigation.navigate("Settings")}
-              >
-                <Image
-                  style={styles.postImage}
-                  source={require("../Assets/user.png")}
-                />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.postsRow}>
-              <Image
-                style={styles.postImage}
-                source={require("../Assets/user.png")}
-              />
-              <Image
-                style={styles.postImage}
-                source={require("../Assets/user.png")}
-              />
-            </View>
+            <Image
+              style={styles.postImage}
+              source={require("../Assets/user.png")}
+            />
           </View>
         </View>
-      </SafeAreaView>
-    );
-  }
-}
-
+      </View>
+    </SafeAreaView>
+  );
+};
+export default Profile;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
