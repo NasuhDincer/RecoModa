b6287a0b125dee2e42a05f9c0a8fdac365ce367f
DESKTOP-0JSGHSD\nasuh <nasuh.dincer@ug.bilkent.edu.tr>
2023-05-18 17:33:48 +0300

    Search Page fixed

--- RecoModa/Frontend/components/GetImages.js
+++ RecoModa/Frontend/components/GetImages.js
@@ -1,5 +1,5 @@
-import React, { Component, useEffect,useState } from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, View } from "react-native";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import rawipv4 from "../ipv4.json";
@@ -9,57 +9,54 @@ const Imaa = (props) => {
   const user = useSelector((state) => state.user.currentUser);
 
   useEffect(() => {
-   handleSubmit();
-    
+    handleSubmit();
   }, []);
 
   const handleSubmit = async () => {
-    
     try {
       const ipv4Address = rawipv4["ip"];
-      //console.log("post : ",props.post.mediaId)
       const res = await axios.get(
         "http://" + ipv4Address + `:5000/api/media/media/${props.imaa.mediaId}`
       );
     } catch (error) {
-      // handle error response
       console.log(error);
     }
-
-    //props.navigation.navigate("Home")
   };
-return (<>
-  <View
-    style={{
-      padding: "2%",
-      width: `100%`,
-      height: undefined,
-      aspectRatio: 1,
-      marginBottom: "5%",
-    }}
-  >
+
+  return (
     <View
       style={{
-        height: "10%",
+        //padding: "2%",
         width: "100%",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
+        height: "100%",
+        // marginTop: 10,
+        // aspectRatio: 1,
+        //marginBottom: "5%",
+        // marginTop: "2%", // Adjust the marginTop value as needed
       }}
     >
-    </View>
-    <View style={{ width: "100%", height: "60%" }}>
-      <Image
-        source={{ uri: `data:image/png;base64,${props.imaa.img[0].data}` }}
+      <View
         style={{
+          // height: "1%", 
           width: "100%",
-          height: undefined,
-          aspectRatio: 1.7,
-          resizeMode: "contain",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
         }}
-      />
+      ></View>
+      <View style={{ flex: 1 }}>
+        <Image
+          source={{ uri: `data:image/png;base64,${props.imaa.img[0].data}` }}
+          style={{
+            flex: 1,
+            width: "100%",
+            height: undefined,
+            resizeMode: "contain",
+          }}
+        />
+      </View>
     </View>
-  </View>
-  </>);
-}
+  );
+};
+
 export default Imaa;
--- RecoModa/Frontend/screens/SearchContent.js
+++ RecoModa/Frontend/screens/SearchContent.js
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import Imaa from "../components/GetImages";
 import rawipv4 from "../ipv4.json";
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const SearchContent = ({ setShowCamera }) => {
   const navigation = useNavigation();
@@ -35,29 +35,27 @@ const SearchContent = ({ setShowCamera }) => {
     //props.navigation.navigate("Home")
   };
 
+  const windowWidth = Dimensions.get('window').width;
+
   return (
     <View style={{ flex: 1 }}>
       <MasonryList
         data={data}
         numColumns={2}
-        contentContainerStyle={{
-          alignSelf: "stretch",
-          // backgroundColor: "black"
-        }}
         showsVerticalScrollIndicator={false}
         renderItem={({ item, index }) => (
-          <View style={styles.searchImageContainer}>
           <TouchableOpacity
             onPress={() => navigation.navigate("ShowPost", { postId: item })}
-            // style={{ flexBasis: "49%", }}
+            style={styles.searchImageContainer}
           >
-            <Imaa imaa={item} />
+            <Imaa imaa={item} style={{ ...styles.image, width: windowWidth * 0.6,}} />
           </TouchableOpacity>
-          </View>
         )}
         keyExtractor={(item, key) => item._id}
         onEndReachedThreshold={0.5}
         ListFooterComponent={<View style={{ height: 100 }} />}
+        columnWrapperStyle={styles.columnWrapper}
+        spacing={10}
       />
     </View>
   );
@@ -67,7 +65,24 @@ export default SearchContent;
 
 const styles = StyleSheet.create({
   searchImageContainer: {
-    width: "100%",
-   // backgroundColor: "black",
+    flex: 1,
+    aspectRatio: 0.70,
+    borderRadius: 10,
+    overflow: 'hidden',
+    // backgroundColor: "black",
+    borderRadius: 10,
+    // borderWidth: 2,
+    // borderColor: "black",
+    // margin: 5,
+    marginBottom: 5,
+    marginRight: 5,
+    marginLeft: 5,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  columnWrapper: {
+    justifyContent: 'space-between',
   },
 });
